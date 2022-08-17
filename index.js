aa2016bee60e9e57a71d59835f2c1e83ef521db0
gracekyalo <gracendanu27@gmail.com>
2022-08-17 23:15:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide= function(){
+    return 2000 / 100;
+}
+divide();
+
+// arrow function
+const square = x => x * x;
+
+// arrow function takes two parameters
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add();
